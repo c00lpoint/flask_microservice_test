167d028e6ba72618b2d0d828938918d2c1bb96d9
will.c <will.c@hotmail.com>
2019-11-10 15:07:10 +0800

    correct tmdb find movie service url

--- movie_data_service/constants.py
+++ movie_data_service/constants.py
@@ -1,5 +1,5 @@
 class ServiceUrls:
-    TMDB_MOVIE_SEARCH_URL_FORMAT = 'https://api.themoviedb.org/3/movie/%d?api_key=02f3c125eb4f47a6f1daf49c91650b14__'
+    TMDB_MOVIE_SEARCH_URL_FORMAT = 'https://api.themoviedb.org/3/movie/%d?api_key=02f3c125eb4f47a6f1daf49c91650b14'
 
 
 class TmdbMovieProperties:
